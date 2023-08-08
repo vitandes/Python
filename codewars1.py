7bf3aa11e87ff7a93201e35439be0446edee8b76
Victor <andres151998@gmail.com>
2023-08-08 15:50:54 -0500

    codewars1

---
+++ codewars1.py
@@ -0,0 +1,15 @@
+def to_jaden_case(string):
+    a = string.split(" ")
+    b = []
+    
+    for palabra in a:
+        palabra = palabra.capitalize()
+        b.append(palabra)
+        
+    b = " ".join(b)
+    
+    return b
+    
+     
+a =  to_jaden_case('KOgxLVr oxFG HOPW u rFYSnvbpPQ li Rz kDZNSmkSq kB BFWUBL wQ uwFbWDzb NCGSXQ CvNn UEigPO n XvRApXCYQ ZKFoPB XuvNmdph ccYRf P pzP nx epWkkxND JwhhRl nMxdtw s wpi gnWJVk eVpnzsVuN vmXZ MqvvKOne OQLaEXVswS i HTWCH UjY ShXCDQ oMA mf ZsiPGv vd mwvBiXvelp kVU SdGxkMSU Xbktv NrTZCJu MIZlSfcKSG ejVCE Nsoto PdWEvVPs Hz adn Ht jiEd qLks fzlx aurznMiJfM HKO ilFKJFtaH BFbBaSTR yaVvHaZM aIpUJmkm J LtbVfY ZwI YLUfPEphTL TCb ixPMDCeG t wdjdHhz y NH OcrxCqDYgl lvToRVniGu Sim aOwAQDvbz jrZsMI cY RlYofCJiZS XarLgDAbhI NlMyBcLJ qUCDTjuiO GmUEGCAPLb rGsUSg f DSixXP KvMvhtMT UhP qFGIQLBFj')
+print(a)    
